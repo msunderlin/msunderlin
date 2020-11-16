3247490f73386b6b028ae4fa1f16e1199bf3f220
Michael Sunderlin <msunderlin@gmail.com>
2020-11-15 21:40:49 -0800

    recreating router

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
-<?php declare(strict_types=1);
+<?php
 
-require __DIR__.'/../src/Bootstrap.php';
+echo "hello from the ".__DIR__."/index.php file <br/>";
+$params = parse_url($_SERVER['REQUEST_URI']);
+var_dump($params);
+
+parse_str($params['query'],$query);
+$params = str_replace('index.php','',$params);
+$params = explode('/',$params['path']);
+$params = array_values(array_filter($params));
 
---
+++ public/testing/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "hello from the ".__DIR__."/index.php file <br/>";
+var_dump($_GET);
+var_dump($_POST);
